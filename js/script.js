db57c126484525e1b66ad7c515a4209d554c4935
AlexViti <alexviti12@gmail.com>
2022-02-23 14:15:19 +0100

    Resolve exercise

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const boxesContainer = document.querySelector('.boxes-container');
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 	boxesContainer.innerHTML += `<div class="box"></div>`;
-	// if (i % 15 == 0) {
-	//   fizzbuzz
-	// } else if (i % 5 == 0) {
-	// 	buzz
-	// } else if (i % 3 == 0) {
-	//   fizz
-	// } else {
-	// 	i
-	// }
+	let box = boxesContainer.querySelector('.box:last-child');
+	if (i % 15 == 0) {
+	  box.classList.add('fizz-buzz');
+	  box.innerHTML = `fizzbuzz`
+	} else if (i % 5 == 0) {
+		box.classList.add('buzz');
+		box.innerHTML = `buzz`
+	} else if (i % 3 == 0) {
+		box.classList.add('fizz');
+		box.innerHTML = `fizz`
+	} else {
+		box.innerHTML = `${i}`
+	}
   }
