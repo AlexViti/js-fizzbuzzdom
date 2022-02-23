bdf12d3aa817274778633137d481f9979503c00c
AlexViti <alexviti12@gmail.com>
2022-02-23 16:00:33 +0100

    Change style - reduce script lines of code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 const boxesContainer = document.querySelector('.boxes-container');
 
 for (let i = 1; i <= 100; i++) {
-	boxesContainer.innerHTML += `<div class="box"></div>`;
+
+	boxesContainer.innerHTML += `<div class="box">${i}</div>`;
 	let box = boxesContainer.querySelector('.box:last-child');
+
 	if (i % 15 == 0) {
-	  box.classList.add('fizz-buzz');
-	  box.innerHTML = `fizzbuzz`
+
+	  box.classList.add('fizz', 'buzz');
+	  box.innerHTML = `fizzbuzz`;
+
 	} else if (i % 5 == 0) {
 		box.classList.add('buzz');
-		box.innerHTML = `buzz`
+		box.innerHTML = `buzz`;
+
 	} else if (i % 3 == 0) {
 		box.classList.add('fizz');
-		box.innerHTML = `fizz`
-	} else {
-		box.innerHTML = `${i}`
+		box.innerHTML = `fizz`;
 	}
   }
