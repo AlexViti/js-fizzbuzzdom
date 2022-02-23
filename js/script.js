26f067273b5ae07bf4ada247819db7cddf647aae
AlexViti <alexviti12@gmail.com>
2022-02-23 14:07:23 +0100

    Add box creator

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-// for (let i = 0; i < 100; i++) {
-// 	if (i % 15 == 0) {
-// 	  fizzbuzz
-// 	} else if (i % 5 == 0) {
-// 		buzz
-// 	} else if (i % 3 == 0) {
-// 	  fizz
-// 	} else {
-// 		i
-// 	}
-//   }
+const boxesContainer = document.querySelector('.boxes-container');
+
+for (let i = 0; i <= 100; i++) {
+	boxesContainer.innerHTML += `<div class="box"></div>`;
+	// if (i % 15 == 0) {
+	//   fizzbuzz
+	// } else if (i % 5 == 0) {
+	// 	buzz
+	// } else if (i % 3 == 0) {
+	//   fizz
+	// } else {
+	// 	i
+	// }
+  }
